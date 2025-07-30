4666f9e546ec102327becaa414035c89489efdaa
Yurie Ando <yu.lily.au13@gmail.com>
2025-07-30 13:56:40 +0900

    dtoアノテーション修正

--- src/main/java/com/example/BankApp/dto/AmountRequest.java
+++ src/main/java/com/example/BankApp/dto/AmountRequest.java
@@ -1,11 +1,11 @@
 package com.example.BankApp.dto;
 
 import jakarta.validation.constraints.Min;
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
 
 public class AmountRequest {
 
